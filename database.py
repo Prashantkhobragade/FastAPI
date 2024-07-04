7cfed74745d123d7b9de8fe9d312adc18921835e
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-07-04 09:22:56 +0530

    database file updated

--- database.py
+++ database.py
@@ -1,42 +1,60 @@
 import os
 import psycopg2
-from psycopg2.extras import DictCursor
-from dotenv import load_dotenv
+from psycopg2 import pool
+from cfenv import AppEnv
+from fastapi import HTTPException
 
+def connect_to_database():
+    try:
+        env = AppEnv()
+        sql_service = 'postgresql-db'
+        sql = env.get_service(label=sql_service)
+        db_credentials = {
+            "user": sql.credentials['username'],
+            "password": sql.credentials['password'],
+            "host": sql.credentials['hostname'],
+            "port": sql.credentials['port'],
+            "database": sql.credentials['dbname']
+        }
+        
+        connection_pool = psycopg2.pool.SimpleConnectionPool(1, 20, **db_credentials)
+        if connection_pool:
+            connection = connection_pool.getconn()
+            return connection
+        else:
+            raise HTTPException(status_code=500, detail="Error creating connection pool")
+    except (Exception, psycopg2.Error) as error:
+        raise HTTPException(status_code=500, detail=f"Error Connecting to DB: {error}")
 
-load_dotenv()
+def execute_create_queries(connection, create_queries):
+    try:
+        cursor = connection.cursor()
 
+        for query in create_queries:
+            cursor.execute(query)
+            print(f"Query executed: {query}")
 
-DB_HOST = os.environ["DB_HOST"]
-DB_NAME = os.environ["DB_NAME"]
-DB_USER = os.environ["DB_USER"]
-DB_PASSWORD = os.environ["DB_PASSWORD"]
+        connection.commit()
+        print("All create queries executed successfully")
+    except (Exception, psycopg2.Error) as error:
+        raise HTTPException(status_code=500, detail=f"Error executing query: {error}")
+    finally:
+        if cursor:
+            cursor.close()
+        connection_pool.putconn(connection)
 
-conn = psycopg2.connect(
-    host=DB_HOST,
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    cursor_factory=DictCursor,
-)
-
-cur = conn.cursor()
-
-def create_table():
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) NOT NULL,
-            email VARCHAR(100) NOT NULL
-        );
-    """)
-    conn.commit()
-
-def insert_into_table(user):
-    cur.execute("""
-        INSERT INTO users (username, email)
-        VALUES (%s, %s);
-    """, 
-    (user.username, user.email)
-    )
-    conn.commit()
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        if cursor.description:
+            result = cursor.fetchall()
+            result_json = json.dumps(result)
+            return result_json
+        else:
+            return "Query executed successfully"
+    except psycopg2.Error as error:
+        raise HTTPException(status_code=500, detail=f"Error executing query: {error}")
+    finally:
+        cursor.close()
+        connection_pool.putconn(connection)
