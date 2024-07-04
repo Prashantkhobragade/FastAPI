e1cc56fff033f71072b0bafbc98262146c60b150
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-07-04 09:19:51 +0530

    database updated

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from database import create_table, insert_into_table
-from model import User
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from database import connect_to_database, execute_create_queries, execute_query
 
 app = FastAPI()
 
-class UserRequest(BaseModel):
-    username: str
-    email: str
+@app.get("/provision")
+def provision(request: Request):
+    create_queries = [
+        # Create table queries
+    ]
 
-@app.post("/users/")
-async def create_user(user: UserRequest):
-    try:
-        user_object = User(username=user.username, email=user.email)
-        insert_into_table(user_object)
-        return {"message": "User created successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    connection = connect_to_database()
 
-@app.get("/users/")
-async def get_users():
+    if connection:
+        execute_create_queries(connection, create_queries)
+        return HTMLResponse("Provisioning completed and PostgreSQL connection is closed")
+    else:
+        return HTMLResponse("Error creating Db connection")
+
+@app.post("/run-query")
+async def run_query(request: Request):
     try:
-        users = []
-        # TO DO: implement fetching users from database
-        return {"users": users}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        query_data = await request.json()
+        query = query_data.get("query")
+        if not query:
+            raise HTTPException(status_code=400, detail="Query parameter is missing")
+
+        connection = connect_to_database()
+        result = execute_query(connection, query)
+        return HTMLResponse(content=result)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {error}")
